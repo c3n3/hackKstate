ce69b430a508130010abbc98b202c4bce95fd463
Grant <g2boehringer@gmail.com>
2019-11-09 16:50:35 -0600

    Update output.py

--- output.py
+++ output.py
@@ -8,10 +8,10 @@ class output():
     def __init__(self):
         self.lcd_rs        = digitalio.DigitalInOut(board.D26)
         self.lcd_en        = digitalio.DigitalInOut(board.D19)
-        self.lcd_d4        = digitalio.DigitalInOut(board.D27)
-        self.lcd_d5        = digitalio.DigitalInOut(board.D22)
-        self.lcd_d6        = digitalio.DigitalInOut(board.D24)
-        self.lcd_d7        = digitalio.DigitalInOut(board.D25)
+        self.lcd_d7        = digitalio.DigitalInOut(board.D27)
+        self.lcd_d6        = digitalio.DigitalInOut(board.D22)
+        self.lcd_d5        = digitalio.DigitalInOut(board.D24)
+        self.lcd_d4        = digitalio.DigitalInOut(board.D25)
         self.lcd_columns = 16
         self.lcd_rows = 2
         self.lcd = characterlcd.Character_LCD_Mono(self.lcd_rs, self.lcd_en, self.lcd_d4, self.lcd_d5, self.lcd_d6, self.lcd_d7, self.lcd_columns, self.lcd_rows)
