d3d356be1161b5b4eaed546eaaf98968bf8b8099
Grant <g2boehringer@gmail.com>
2019-11-09 16:36:26 -0600

    Update output.py

--- output.py
+++ output.py
@@ -1,20 +1,20 @@
 # @see https://pimylifeup.com/i-lcd-16x2/
 
-import Adafruit_CharLCD as LCD
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as characterlcd
 
 class output():
     def __init__(self):
-        self.lcd_rs        = 25
-        self.lcd_en        = 24
-        self.lcd_d4        = 23
-        self.lcd_d5        = 17
-        self.lcd_d6        = 18
-        self.lcd_d7        = 22
-        self.lcd_backlight = 4
+        self.lcd_rs        = digitalio.DigitalInOut(board.D26)
+        self.lcd_en        = digitalio.DigitalInOut(board.D26)
+        self.lcd_d4        = digitalio.DigitalInOut(board.D26)
+        self.lcd_d5        = digitalio.DigitalInOut(board.D26)
+        self.lcd_d6        = digitalio.DigitalInOut(board.D26)
+        self.lcd_d7        = digitalio.DigitalInOut(board.D26)
         self.lcd_columns = 16
         self.lcd_rows = 2
-        self.lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-
+        self.lcd = characterlcd.Character_LCD_Mono(self.lcd_rs, self.lcd_en, self.lcd_d4, self.lcd_d5, self.lcd_d6, self.lcd_d7, self.lcd_columns, self.lcd_rows)
     def showAnswer(self, string):
         self.lcd.set_cursor(16 - len(string), 1)
         self.lcd.message = string
